262aa51981bfb1c560abd8868725d1a3e35b0bda
BridgerB <6395506+BridgerB@users.noreply.github.com>
2025-02-21 20:27:13 -0700

    moved container to flake

--- flake.nix
+++ flake.nix
@@ -7,17 +7,58 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_23
-            xdotool
-          ];
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_23
+          xdotool
+        ];
+      };
+    }) // {
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ pkgs, ... }: {
+            system.stateVersion = "23.11";
+            boot.isContainer = true;
+
+            environment.systemPackages = with pkgs; [
+              nodejs_23
+            ];
+
+            users.users.agent = {
+              isNormalUser = true;
+              password = "agent";
+              extraGroups = ["wheel"];
+            };
+
+            security.sudo.wheelNeedsPassword = false;
+          })
+        ];
+      };
+
+      apps.x86_64-linux = {
+        start-container = {
+          type = "app";
+          program = toString (nixpkgs.legacyPackages.x86_64-linux.writeScript "start-container" ''
+            #!${nixpkgs.legacyPackages.x86_64-linux.bash}/bin/bash
+            set -e
+            sudo nixos-container destroy agent || true
+            sudo nixos-container create agent --flake .#container
+            sudo nixos-container start agent
+          '');
         };
-      }
-    );
+
+        stop-container = {
+          type = "app";
+          program = toString (nixpkgs.legacyPackages.x86_64-linux.writeScript "stop-container" ''
+            #!${nixpkgs.legacyPackages.x86_64-linux.bash}/bin/bash
+            sudo nixos-container stop agent
+            sudo nixos-container destroy agent
+          '');
+        };
+      };
+    };
 }
