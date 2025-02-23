f8b08cb4b1557f6c2d6f5800decd653ef009e9b7
BridgerB <6395506+BridgerB@users.noreply.github.com>
2025-02-22 17:34:09 -0700

    added vscode to agent container

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "LLM agent with mouse control capabilities";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -13,7 +11,6 @@
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           nodejs_23
-
         ];
       };
     }) // {
@@ -23,13 +20,35 @@
           ({ pkgs, ... }: {
             system.stateVersion = "23.11";
             boot.isContainer = true;
-
+            
+            networking = {
+              firewall = {
+                enable = true;
+                allowedTCPPorts = [ 8080 ];
+              };
+              hostName = "agent";
+            };
+            
             environment.systemPackages = with pkgs; [
               nodejs_23
-          xdotool
-          toybox
-          tree
+              xdotool 
+              toybox
+              tree
             ];
+
+            services.openvscode-server = {
+              enable = true;
+              user = "agent";
+              group = "users";
+              host = "0.0.0.0";
+              port = 8080;
+              withoutConnectionToken = true;
+              extraPackages = with pkgs; [
+                git
+                nodejs
+                yarn
+              ];
+            };
 
             users.users.agent = {
               isNormalUser = true;
@@ -51,6 +70,7 @@
             sudo nixos-container destroy agent || true
             sudo nixos-container create agent --flake .#container
             sudo nixos-container start agent
+            echo "VSCode server available at http://10.233.1.2:8080"
           '');
         };
 
